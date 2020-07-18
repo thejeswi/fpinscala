e8422b3df2866a42e831828afbf72913a1bc2a6b
Thejeswi Preetham Nagendra Kamatchi <preetham.nagendra@holidaycheck.com>
2020-07-18 11:26:53 +0200

    Fix function to make it tail recurrsive

--- src/main/scala/exercises/chapter2/IsSorted.scala
+++ src/main/scala/exercises/chapter2/IsSorted.scala
@@ -4,8 +4,8 @@ object IsSorted {
   def run[A](as: Array[A], ordered: (A,A) => Boolean): Boolean = {
     def loop(n: Int): Boolean =
       if (n >= as.length-1) true
-      else if (ordered(as(n), as(n+1))) loop(n+1)
-      else false
+      else if (!ordered(as(n), as(n+1))) false
+      else loop(n+1)
    loop(0)
   }
 }
